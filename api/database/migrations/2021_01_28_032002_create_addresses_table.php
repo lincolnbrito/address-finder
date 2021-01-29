ae495c27efa1b76f2e0fd03ea3a189ad1c131f54
lincolnbrito <lincoln.sbrito@gmail.com>
2021-01-28 23:53:40 -0300

    create address structure

---
+++ api/database/migrations/2021_01_28_032002_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('zipcode', 10);
+            $table->string('street');
+            $table->string('neighborhood');
+            $table->string('state', 2);
+            $table->string('city');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
